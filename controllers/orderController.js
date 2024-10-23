4ed3ef676763bb6b861b926cadac6519fe339cd8
Majorbangz88 <joelchimaobichukwu@gmail.com>
2024-10-23 15:58:21 +0100

    place order with cash on delivery endpoint logic added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,10 +1,31 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
 
 
 const placeOrderCash = async (req, res) => {
     try {
+        const { userId, items, amount, address } = req.body;
+
+        const orderData = {
+            userId,
+            items,
+            address
+            amount,
+            paymentMethod: 'COD',
+            payment: false,
+            date: Date.now(),
+        }
+
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();
+
+        await userModel.findByIdAndUpdate(userId, {orderData: {}})
+
+        res.json({success: true, message: 'Order Placed'});
 
     } catch (error) {
-
+        console.log(error)
+        res.json({success: false, message: error.message});
     }
 }
 
